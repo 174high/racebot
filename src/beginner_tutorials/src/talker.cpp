593f8305bb5e3646a8ba3fa1c459ae012b3d8ea3
174high <466243346@qq.com>
2016-02-12 05:59:00 +0800

    add position control

--- src/beginner_tutorials/src/talker.cpp
+++ src/beginner_tutorials/src/talker.cpp
@@ -9,7 +9,9 @@ int main(int argc, char **argv)
 
 	ros::NodeHandle n;  
 
-	ros::Publisher chatter_pub = n.advertise<std_msgs::Float64>("/vesc/commands/motor/speed", 10);  
+	ros::Publisher speed_pub = n.advertise<std_msgs::Float64>("/vesc/commands/motor/speed", 10);  
+        ros::Publisher position_pub = n.advertise<std_msgs::Float64>("/vesc/commands/servo/position", 10);
+
 
 	ros::Rate loop_rate(10);  
 
@@ -23,13 +25,16 @@ int main(int argc, char **argv)
 	/** 
 	 * This is a message object. You stuff it with data, and then publish it. 
 	 */  
-	std_msgs::Float64::Ptr  msg(new std_msgs::Float64);  
+	std_msgs::Float64::Ptr  msg_s(new std_msgs::Float64);  
+        std_msgs::Float64::Ptr  msg_p(new std_msgs::Float64);
 
 //	std::stringstream ss;  
 //	ss << 100 << count;  
-	msg->data = 100;  
+	msg_s->data = 1200;  
+        msg_p->data = 0.8;
 
-	ROS_INFO("%g", msg->data);  
+	ROS_INFO("%g", msg_s->data);  
+        ROS_INFO("%g", msg_p->data);  
 
 	/** 
 	 * The publish() function is how you send messages. The parameter 
@@ -37,7 +42,8 @@ int main(int argc, char **argv)
 	 * given as a template parameter to the advertise<>() call, as was done 
 	 * in the constructor above. 
 	 */  
-	chatter_pub.publish(msg);  
+	speed_pub.publish(msg_s);  
+        position_pub.publish(msg_p);
 
 	ros::spinOnce();  
 
