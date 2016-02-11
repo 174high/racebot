4be063548ccf04c6c483f4251ade7f0f972fa70f
174high <466243346@qq.com>
2016-02-12 07:30:16 +0800

    fix issue

--- src/beginner_tutorials/src/talker.cpp
+++ src/beginner_tutorials/src/talker.cpp
@@ -38,11 +38,113 @@
 #define test_car     50
 #define stop_do      0xff 
 
+int sockfd;
+
+//ros::init(argc, argv, "talker");
+//ros::Rate loop_rate(10);
+
+std_msgs::Float64::Ptr  msg_s(new std_msgs::Float64);
+std_msgs::Float64::Ptr  msg_p(new std_msgs::Float64);
+
+ros::Publisher speed_pub ;
+ros::Publisher position_pub;
+
+char buf[MAXDATASIZE]={0};
+
+int command=0;
+int direction=0;
+float position=0.43;
+
+bool is_training_mode=false ; 
+bool is_testing_mode=false; 
+/**
+the control_car（）function is used to control  motion of car 
+command=run_forward is used to control the direction of car to move forward 
+command=run_backward is used to control to move backward 
+command=turn_right  is used to control to turn right 
+command=turn_left   is used to control to turn left 
+command=kill_car    is used to stop control car 
+command=stop_car    is used to stop car (speed=0)
+command=speed_level_1~10 is used to control the speed of car
+*/
+
+void control_car(int & command)
+{
+
+
+               if(command==run_forward)
+                {
+                    position=0.43;
+                    direction=1;
+                }
+                else if(command==run_backward)
+                {
+                    position=0.43;
+                    direction=-1;
+                }
+                else  if(command==turn_right)
+                {
+                    if(position<=0.8)
+                    position+=0.15;
+                    }
+                else if(command==turn_left)
+                {
+                    if(position>=0.1)
+                    position-=0.15;
+                }
+                else if((command>=speed_level_1)&&(command<=speed_level_10))
+                {
+                      ROS_INFO("command=%d",command);
+                      msg_s->data=(1000+(2250/10)*command);
+
+                }
+                else if(command==kill_car)
+                {
+                        close(sockfd);
+                        exit(1);
+                }
+
+                if(msg_s->data*direction<0)
+                msg_s->data=msg_s->data*-1;
+
+                msg_p->data = position;
+
+                ROS_INFO("%g", msg_s->data);
+                ROS_INFO("%g", msg_p->data);
+
+                if(command==stop_car)
+                {
+                        msg_s->data=0;
+                }
+
+                /** 
+                * The publish() function is how you send messages. The parameter 
+                * is the message object. The type of this object must agree with the type 
+                * given as a template parameter to the advertise<>() call, as was done 
+                * in the constructor above. 
+                */
+
+                if(command!=0)
+                {
+
+                        speed_pub.publish(msg_s);
+
+                        position_pub.publish(msg_p);
+                }
+
+                command=0;
+
+                ros::spinOnce();
+
+                //loop_rate.sleep();
+
+                //memset(buf,0,sizeof(buf));
+
+}
+
+
 int main(int argc, char **argv)  
 {  
-        int command=0;         
-        int direction=0;
-        float position=0.43;
         int reuse = 1;
 
 	ros::init(argc, argv, "talker");  
@@ -53,20 +155,14 @@ int main(int argc, char **argv)
          * This is a message object. You stuff it with data, and then publish it. 
          */
 
-        std_msgs::Float64::Ptr  msg_s(new std_msgs::Float64);
-        std_msgs::Float64::Ptr  msg_p(new std_msgs::Float64);
+	speed_pub = n.advertise<std_msgs::Float64>("/vesc/commands/motor/speed", 10);  
+        position_pub = n.advertise<std_msgs::Float64>("/vesc/commands/servo/position", 10);
 
-	ros::Publisher speed_pub = n.advertise<std_msgs::Float64>("/vesc/commands/motor/speed", 10);  
-        ros::Publisher position_pub = n.advertise<std_msgs::Float64>("/vesc/commands/servo/position", 10);
-
-
-	ros::Rate loop_rate(10);  
-
-       struct sockaddr_in server_sockaddr,client_sockaddr;
+        struct sockaddr_in server_sockaddr,client_sockaddr;
         socklen_t sin_size ;
         //socklen_t sin_size= sizeof(struct sockaddr);
         int recvbytes;
-        int sockfd,client_fd;
+        int client_fd;
         char buf[MAXDATASIZE]={0};
                           //IPV4 xieyi
         if((sockfd = socket(AF_INET,SOCK_STREAM,0))== -1)
@@ -75,11 +171,11 @@ int main(int argc, char **argv)
                 exit(1);
         }
 
-       if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
-       {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+        {
                 perror("setsockopet error\n");
                 return -1;
-       }
+        }
 
         printf("socket success!,sockfd=%d\n",sockfd);
         server_sockaddr.sin_family=AF_INET;
@@ -111,9 +207,12 @@ int main(int argc, char **argv)
                 perror("accept");
                 exit(1);
         }
+ 
 
         while (ros::ok())
         {
+                memset(buf,0,sizeof(buf));
+
                 if((recvbytes=recv(client_fd,buf,100,0))== -1)
                 {
                         perror("recv");
@@ -131,90 +230,25 @@ int main(int argc, char **argv)
         		} 
                 }
 
-//                if(send(client_fd,buf,strlen(buf),0)== -1)
-//                {
-//                        perror("send");
-//                        exit(1);
-//                }
-
                 command=atoi(buf);
-            
-                if(command==run_forward)
+
+    
+                if(command==train_car)
                 {
-                    position=0.43; 
-                    direction=1; 
+                	is_training_mode=true; 
                 }
-                else if(command==run_backward)
+                else if (command==test_car)
                 { 
-                    position=0.43;
-                    direction=-1; 
-                }                   
-                else  if(command==turn_right)
-                {
-                    if(position<=0.8)
-                    position+=0.15;
-                    }
-                else if(command==turn_left)
-                {
-                    if(position>=0.1)
-                    position-=0.15;
-                }              
-                else if((command>=speed_level_1)&&(command<=speed_level_10))
-                {
-                      ROS_INFO("command=%d",command);
-                      msg_s->data=(1000+(2250/10)*command); 
-
+                        is_testing_mode=true; 
                 }
-                else if(command==kill_car)
+                else if(command==stop_do) 
                 {
-                	close(sockfd);
-                        exit(1);
+                	is_training_mode=false;
+                        is_testing_mode= false;
                 }
-                else if(command==train_car)
-                {
+            
+                control_car(command);
                 
-                }
-                else if(command==test_car)
-                {
-
-
-                }
-  
-                if(msg_s->data*direction<0)
-                msg_s->data=msg_s->data*-1;                
-
-        	msg_p->data = position;
-
-		ROS_INFO("%g", msg_s->data);  
-        	ROS_INFO("%g", msg_p->data);  
-
-                if(command==stop_car)
-                {
-                	msg_s->data=0;
-                }
-
-		/** 
-	 	* The publish() function is how you send messages. The parameter 
-	 	* is the message object. The type of this object must agree with the type 
-	 	* given as a template parameter to the advertise<>() call, as was done 
-	 	* in the constructor above. 
-	 	*/  
-                
-                if(command!=0)
-                {
-                        
-			speed_pub.publish(msg_s);  
-
-        		position_pub.publish(msg_p);
-                } 
-              
-                command=0;
-
-		ros::spinOnce();  
-
-		loop_rate.sleep();  
- 
-                memset(buf,0,sizeof(buf));
 	}  
 
 	close(sockfd);
